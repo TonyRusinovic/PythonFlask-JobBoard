16cb9c3a8ce29959fd71d9c1373c30c5ef71d29e
TonyRusinovic <tony.rusinovic@uniface.com>
2019-04-03 07:53:21 +0200

    Update app.py
    Commit should be performed on the cursorinstead of the connection

--- jobs/app.py
+++ jobs/app.py
@@ -15,7 +15,7 @@ def execute_sql(sql,values=(),commit=False,single=False):
 	connection = open_connection()
 	cursor = connection.execute(sql,values)
 	if commit:
-		results = connection.commit()
+		results = cursor.commit()
 	else:
 		results = cursor.fetchone() if single else cursor.fetchall()
 	cursor.close()
